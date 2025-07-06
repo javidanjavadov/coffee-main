621a728d9a4c88144338fd2b1eeccf1862a7a230
javidanjavadov <javidan.javadov@gmail.com>
2025-07-06 22:56:03 +0400

    testing

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
 plugins {
     kotlin("jvm") version "1.9.22"
     id("io.ktor.plugin") version "2.3.7"
+    application
+    id("com.github.johnrengelman.shadow") version "7.1.2"
 }
 
 group = "com.motycka.edu"
 version = "0.0.1"
 
 application {
-    mainClass = "io.ktor.server.netty.EngineMain"
+    mainClass.set("com.motycka.edu.ApplicationKt")
 }
 
 repositories {
